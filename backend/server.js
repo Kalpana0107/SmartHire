399c4e6558a25eca27d2690cc4e679bdce208982
Soumiya-33 <soumiyasunilkumar33@gmail.com>
2026-07-11 16:28:39 +0530

    adding a function for the candidate fetching and deletion data

--- backend/server.js
+++ backend/server.js
@@ -3,27 +3,35 @@ const extractRoutes = require("./routes/extract");
 
 const express = require("express");
 const cors = require("cors");
+
+const candidateRoutes = require("./routes/candidates");
+
 require("dotenv").config();
 
 const app = express();
 
-const PORT =process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 
 app.use(cors());
 app.use(express.json());
 
+app.use(express.json());
+
+
 const healthRoute = require("./routes/health");
 app.use("/health", healthRoute);
 app.use("/api", uploadRoutes);
 app.use("/api", extractRoutes);
+app.use("/api", candidateRoutes);
+
 
 const matchRoute = require("./routes/match");
 app.use("/match", matchRoute);
 
 const db = require("./db/database");
 
-app.listen(PORT , () => {
+app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 
 });
